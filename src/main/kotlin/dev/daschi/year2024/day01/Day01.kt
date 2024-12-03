dd5f15cdfb82edd14c76a03bcf7f8625f7ed98a2
Daschi1 <Daschi1@users.noreply.github.com>
2024-12-03 20:46:06 +0100

    added generated files for 2024 day 01

---
+++ src/main/kotlin/dev/daschi/year2024/day01/Day01.kt
@@ -0,0 +1,30 @@
+package dev.daschi.year2024.day01
+
+import dev.daschi.util.Input
+import dev.daschi.util.Solution
+
+/**
+ * Solution for Day 1 of Advent of Code 2024.
+ */
+class Day01(input: List<String>? = null) : Solution {
+    override val year = 2024
+    override val day = 1
+
+    private val input = input ?: Input.readLines(year, day)
+
+    /**
+     * Solves Part 1.
+     */
+    override fun part1(): Any? {
+        // TODO: Implement Part 1
+        return null
+    }
+
+    /**
+     * Solves Part 2.
+     */
+    override fun part2(): Any? {
+        // TODO: Implement Part 2
+        return null
+    }
+}
