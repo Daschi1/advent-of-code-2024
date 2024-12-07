0e5d706736d50e9ac118c0dfe1701253df71fccd
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2024-12-07 16:31:07 +0100

    add solution for 2024 day 04 part 1

--- src/main/kotlin/dev/daschi/year2024/day04/Day04.kt
+++ src/main/kotlin/dev/daschi/year2024/day04/Day04.kt
@@ -14,17 +14,79 @@ class Day04(
 
     private val parsedInput = parseInput(input)
 
-    private fun parseInput(input: List<String>): List<String> {
-        return input
+    private fun parseInput(input: List<String>): Array<CharArray> {
+        val height = input.size
+        val width = input[0].length
+        val grid = Array(height) { CharArray(width) }
+        for (y in 0 until height) {
+            val line = input[y]
+            for (x in 0 until width) {
+                grid[y][x] = line[x]
+            }
+        }
+        return grid
     }
 
     /**
      * Solves Part 1.
      */
     override fun part1(): Any {
-        // TODO: Implement Part 1
-        return -1
+        val directions = listOf(
+            Pair(1, 0), Pair(-1, 0), // Horizontal
+            Pair(0, 1), Pair(0, -1), // Vertical
+            Pair(1, 1), Pair(-1, -1), // Diagonal down-right, up-left
+            Pair(-1, 1), Pair(1, -1)  // Diagonal down-left, up-right
+        )
+        var result = 0
+
+        val height = parsedInput.size
+        val width = parsedInput[0].size
+        val word = "XMAS"
+
+//        for (y in 0 until height) {
+//            for (x in 0 until width) {
+//                print(parsedInput[y][x])
+//            }
+//            print("\n")
+//        }
+//        println("---")
+
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                for (direction in directions) {
+                    if (matchWordInGrid(y, x, direction.first, direction.second, word)) {
+                        result++
+                    }
+                }
+            }
+        }
+
+        return result
     }
+
+    private fun matchWordInGrid(yStart: Int, xStart: Int, dy: Int, dx: Int, word: String): Boolean {
+        val height = parsedInput.size
+        val width = parsedInput[0].size
+        var y = yStart
+        var x = xStart
+        val traversed = mutableListOf<Pair<Int, Int>>()
+        for (i in word.indices) {
+            traversed.add(Pair(y, x))
+            if (y !in (0 until height) || x !in (0 until width) || word[i] != parsedInput[y][x]) {
+                return false
+            }
+            y += dy
+            x += dx
+        }
+//        println(traversed)
+        return true
+    }
+
+    /*private fun printGrid(grid: Array<CharArray>) {
+        for (row in grid) {
+            println(row.joinToString(""))
+        }
+    }*/
 
     /**
      * Solves Part 2.
--- src/test/kotlin/dev/daschi/year2024/day04/Day04Test.kt
+++ src/test/kotlin/dev/daschi/year2024/day04/Day04Test.kt
@@ -15,7 +15,7 @@ class Day04Test {
         @JvmStatic
         fun part1Samples() = listOf(
             // sampleNumberToLoad, expectedResult
-            Arguments.of(1, -1)
+            Arguments.of(1, 18)
         )
 
         @JvmStatic
