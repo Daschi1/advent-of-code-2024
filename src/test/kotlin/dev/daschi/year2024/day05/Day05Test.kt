e14120c6de8b71db75efdff57e5eed5dbe893303
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2024-12-08 10:55:57 +0100

    add 2024 day 05 template files

---
+++ src/test/kotlin/dev/daschi/year2024/day05/Day05Test.kt
@@ -0,0 +1,43 @@
+package dev.daschi.year2024.day05
+
+import dev.daschi.util.Input
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertEquals
+
+/**
+ * Tests for Day 5 of Advent of Code 2024.
+ */
+class Day05Test {
+
+    companion object {
+        @JvmStatic
+        fun part1Samples() = listOf(
+            // sampleNumberToLoad, expectedResult
+            Arguments.of(1, -1)
+        )
+
+        @JvmStatic
+        fun part2Samples() = listOf(
+            // sampleNumberToLoad, expectedResult
+            Arguments.of(1, -1)
+        )
+    }
+
+    @ParameterizedTest(name = "Part 1 Sample {0}")
+    @MethodSource("part1Samples")
+    fun testPart1Samples(sampleNumber: Int, expected: Any?) {
+        val sampleInput = Input.readLines(2024, 5, sampleNumber)
+        val day = Day05(sampleInput)
+        assertEquals(expected, day.part1())
+    }
+
+    @ParameterizedTest(name = "Part 2 Sample {0}")
+    @MethodSource("part2Samples")
+    fun testPart2Samples(sampleNumber: Int, expected: Any?) {
+        val sampleInput = Input.readLines(2024, 5, sampleNumber)
+        val day = Day05(sampleInput)
+        assertEquals(expected, day.part2())
+    }
+}
