7c0f1e3dd85dea174840cadd97d3902fd4293801
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2024-12-08 11:34:23 +0100

    attempt solution 2024 day 05

--- src/main/kotlin/dev/daschi/year2024/day05/Day05.kt
+++ src/main/kotlin/dev/daschi/year2024/day05/Day05.kt
@@ -12,18 +12,59 @@ class Day05(
     override val year = 2024
     override val day = 5
 
-    private val parsedInput = parseInput(input)
+    private val pageOrderingRules = parsePageOrderingRules(input)
+    private val pageUpdates = parsePageUpdates(input)
 
-    private fun parseInput(input: List<String>): List<String> {
-        return input
+    private fun parsePageOrderingRules(input: List<String>): List<PageOrderingRule> {
+        return input.takeWhile { it.isNotEmpty() } // Process lines until an empty line is encountered
+            .map { line ->
+                val split = line.split("|")
+                val dependency = split[0].toInt()
+                val dependent = split[1].toInt()
+                PageOrderingRule(dependency, dependent)
+            }
     }
+
+    private fun parsePageUpdates(input: List<String>): List<List<Int>> {
+        return input.dropWhile { it.isNotEmpty() } // Skip lines until the first empty line
+            .drop(1) // Drop the empty line itself
+            .map { line ->
+                line.split(",").map { it.toInt() }
+            }
+    }
+
+    private data class PageOrderingRule(val dependency: Int, val dependent: Int)
 
     /**
      * Solves Part 1.
      */
     override fun part1(): Any {
-        // TODO: Implement Part 1
+        println(pageOrderingRules)
+        println(pageUpdates)
+        println(retrieveDependencies(29))
+        println(isPageInValidPosition(pageUpdates[0], 29))
         return -1
+    }
+
+    private fun isPageInValidPosition(pageUpdate: List<Int>, page: Int): Boolean {
+        val pageIndex = pageUpdate.indexOf(page)
+        if (pageIndex == -1) {
+            throw IllegalArgumentException("Page number '$page' is not present in pageUpdate $pageUpdate.")
+        }
+
+        val dependencies = retrieveDependencies(page)
+        // Iterate over pages before the position of the target page
+        for (priorPage in pageUpdate.take(pageIndex)) {
+            if (priorPage in dependencies) {
+                // remove?
+            }
+        }
+
+        return emptyList()
+    }
+
+    private fun retrieveDependencies(page: Int): List<Int> {
+        return pageOrderingRules.filter { it.dependent == page }.map { it.dependency }
     }
 
     /**
--- src/test/kotlin/dev/daschi/year2024/day05/Day05Test.kt
+++ src/test/kotlin/dev/daschi/year2024/day05/Day05Test.kt
@@ -15,7 +15,7 @@ class Day05Test {
         @JvmStatic
         fun part1Samples() = listOf(
             // sampleNumberToLoad, expectedResult
-            Arguments.of(1, -1)
+            Arguments.of(1, 143)
         )
 
         @JvmStatic
