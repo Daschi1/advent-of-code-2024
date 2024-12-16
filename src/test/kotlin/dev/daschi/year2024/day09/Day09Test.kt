449fa382298579b43f8287cdfc20b6bc823e3611
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2024-12-16 09:12:01 +0100

    add test result for 2024 day 09 part 1

--- src/test/kotlin/dev/daschi/year2024/day09/Day09Test.kt
+++ src/test/kotlin/dev/daschi/year2024/day09/Day09Test.kt
@@ -15,7 +15,7 @@ class Day09Test {
         @JvmStatic
         fun part1Samples() = listOf(
             // sampleNumberToLoad, expectedResult
-            Arguments.of(1, -1)
+            Arguments.of(1, 1928)
         )
 
         @JvmStatic
