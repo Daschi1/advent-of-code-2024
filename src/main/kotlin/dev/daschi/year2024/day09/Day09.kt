e94a2e858de2ab4d96696e5246e45953a1773e78
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2024-12-16 09:10:48 +0100

    add template files for 2024 day 09

---
+++ src/main/kotlin/dev/daschi/year2024/day09/Day09.kt
@@ -0,0 +1,36 @@
+package dev.daschi.year2024.day09
+
+import dev.daschi.util.Input
+import dev.daschi.util.Solution
+
+/**
+ * Solution for Day 9 of Advent of Code 2024.
+ */
+class Day09(
+    input: List<String> = Input.readLines(2024, 9)
+) : Solution {
+    override val year = 2024
+    override val day = 9
+
+    private val parsedInput = parseInput(input)
+
+    private fun parseInput(input: List<String>): List<String> {
+        return input
+    }
+
+    /**
+     * Solves Part 1.
+     */
+    override fun part1(): Any {
+        // TODO: Implement Part 1
+        return -1
+    }
+
+    /**
+     * Solves Part 2.
+     */
+    override fun part2(): Any {
+        // TODO: Implement Part 2
+        return -1
+    }
+}
